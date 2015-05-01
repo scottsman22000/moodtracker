ee1316f5acb232fb88b1361c8ec7a42664c519d0
ecsandle <ecsandle@umass.edu>
2015-05-01 14:29:43 -0400

    I Added The log moods page as a new activity

--- app/src/main/java/com/example/mike/moodtracker/LogMoods.java
+++ app/src/main/java/com/example/mike/moodtracker/LogMoods.java
@@ -5,21 +5,22 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class LogMoods extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log_moods); //I took this out we will see if it works
+       // Intent intent = getIntent();//I was supposed to add this
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_log_moods, menu);
         return true;
     }
 
@@ -36,10 +37,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void createLogMoodsPage(View view){
-        Intent intent = new Intent(this, LogMoods.class);//i need to add the kind of activity that i want
-        startActivity(intent);
     }
 }
