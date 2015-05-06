71e5f2b9f055c4c1db2069c5d9a820a25b2e521c
mikecmeli <mikecmeli@gmail.com>
2015-05-06 17:29:48 -0400

    Added MoodData and its necessary classes.

--- app/src/main/java/com/example/mike/moodtracker/GraphBuilder.java
+++ app/src/main/java/com/example/mike/moodtracker/GraphBuilder.java
@@ -6,14 +6,33 @@ import android.app.Activity;
 
 public class GraphBuilder extends Activity{
 
-    MoodData[] moodData;
+    ArrayList<MoodData> moodData;
+    Date startTime;
+    Date endTime;
 
     public MoodData getData(Date startTime, Date endTime, int graphType, Mood mood){
         //pull mood data between two times and of matching mood and stores them in mood data
+        ArrayList<MoodData> moodData = null;//import data
+        //filter data by mood
+        for (MoodData m : moodData) {
+            if (!m.mood.mood.equals(mood.mood))
+                moodData.remove(m);
+        }
+        this.moodData = moodData;
+        this.startTime = startTime;
+        this.endTime = endTime;
         return null;
     }
 
     public void graphPoints(){
-        //graph points 
+        //graph points
+        long range = endTime.getTime() - startTime.getTime();
+        ArrayList<Float> horizontalPosition = new ArrayList<Float>();
+        for (MoodData m : moodData) {
+            horizontalPosition.add((float) ((m.timeStamp.getTimeInMillis() - startTime.getTime()) / range));
+        }
+        //horizontal positions are stored in horizontalPosition from 0.0 to 1.0
+        //vertical positions are stored in moodData.mood.intensity from 0.0 to 1.0
+        //find a graphing tool that lets you do this
     }
 }
