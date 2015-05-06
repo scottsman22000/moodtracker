22261280617e1231d54c4f9268915a6018c55372
mikecmeli <mikecmeli@gmail.com>
2015-05-06 15:35:28 -0400

    Added MoodData and its necessary classes.

---
+++ app/src/main/java/com/example/mike/moodtracker/CopingStrategy.java
@@ -0,0 +1,14 @@
+package com.example.mike.moodtracker;
+
+/**
+ * Created by Michael on 5/5/2015.
+ */
+public class CopingStrategy {
+    String copingStrategy;
+    int rating;
+
+    public CopingStrategy(String copingStrategy, int rating) {
+        this.copingStrategy = copingStrategy;
+        this.rating = rating;
+    }
+}
