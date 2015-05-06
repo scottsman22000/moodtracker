92b149acbf510a71f1f5fe4a6e364e219630a561
Bradley Swift <braddars@gmail.com>
2015-05-05 22:04:36 -0400

    Skeleton code created for GraphBuilder

---
+++ app/src/main/java/com/example/mike/moodtracker/GraphBuilder.java
@@ -0,0 +1,17 @@
+package com.example.mike.moodtracker;
+
+import java.util.*;
+
+public class GraphBuilder extends Activity{
+
+    MoodData[] moodData;
+
+    public MoodData getData(Date startTime, Date endTime, int graphType, Mood mood){
+        //pull mood data between two times and of matching mood and stores them in mood data
+        return null;
+    }
+
+    public void graphPoints(){
+        //graph points
+    }
+}
